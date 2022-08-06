db73f89e574d1276aa42cf8566458c46415d5a39
Apoorv <apoorvapainal@gmail.com>
2022-08-06 21:39:34 +0530

    Added a program to add two integers entered by user

---
+++ add_int.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int first_int, second_int;
+
+    printf("Enter the first integer: ");
+    scanf("%d",&first_int);
+
+    printf("Enter the second integer: ");
+    scanf("%d",&second_int);
+
+    printf("Sum of the two integers is: %d \n",first_int+second_int);
+
+    return 0;
+}
